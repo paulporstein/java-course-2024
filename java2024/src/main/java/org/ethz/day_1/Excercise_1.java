ded1cbf8817b270c94475125b88c02d96f591c33
paulporstein <122795655+paulporstein@users.noreply.github.com>
2024-09-10 15:01:46 +0200

    nice

---
+++ java2024/src/main/java/org/ethz/day_1/Excercise_1.java
@@ -0,0 +1,18 @@
+package org.ethz.day_1;
+import java.util.Scanner;
+
+public class Excercise_1 {
+    
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int userInput = input.nextInt();
+        System.out.println("Input integer: "+userInput);
+        
+        
+
+
+
+
+    }
+}
